f1630fa743024094a71fccbcec1b7cd109e13ec1
osaid <osaidakrei@gmail.com>
2023-11-07 03:48:31 +0300

    added delay between messages

--- index.js
+++ index.js
@@ -5,106 +5,143 @@ require("dotenv").config();
 const HOST = process.env.HOST_NAME;
 const USER = process.env.USER_NAME;
 const PASS = process.env.PASS_KEY;
-const ROOMID = process.env.ROOM_ID;
+let ROOMID;
 
 const SSL = true; // server uses https ?
 const ROOMS = [process.env.ROOM_NAME];
-
 const TelegramBot = require("node-telegram-bot-api");
+
+let loggedIn = false;
+let callIt = true;
+const delayTime = 1500;
+const messagesQueue = [];
+const { sendMessage } = driver;
+const { sendToRoom } = driver;
 
 // TELEGRAM TOKEN
 const token = process.env.TOKEN_KEY;
 
 const bot = new TelegramBot(token, { polling: true });
 
-function callBot(msg) {
-  const chatId = msg.chat.id;
-  sendMessage(msg);
-  bot.sendMessage(chatId, "Sending your message to Rocket.Chat");
-}
+// CONNTECT TO ROCKET.CHAT WORKSPACE
+const runbot = async () => {
+  await driver.connect({ host: HOST, useSsl: SSL });
+  await driver.login({ username: USER, password: PASS });
+  await driver.joinRooms(ROOMS);
+  ROOMID = await getRoomId(ROOMS[0]);
+  await driver.subscribeToMessages();
+  loggedIn = true;
+};
 
 // DETECT ANY MESSAGE IN TELEGRAM GROUPS
 bot.on("message", (msg) => {
-  callBot(msg);
+  if (!loggedIn) return;
+  messagesQueue.push(msg);
+  prepareMessage();
 });
 
 // DETECT ANY MESSAGE IN TELEGRAM CHANNELS
 bot.on("channel_post", (msg) => {
-  callBot(msg);
+  if (!loggedIn) return;
+  messagesQueue.push(msg);
+  prepareMessage();
 });
 
-var myuserid;
+// PREPARE TO SEND THE MESSAGE // DELAY WHEN THERE'S MULTIPLE REQUESTS
+function prepareMessage() {
+  if (callIt) {
+    msg = messagesQueue[0];
+    const chatId = msg.chat.id;
+    bot.sendMessage(chatId, "Sending your message to Rocket.Chat");
+    sendToRocket(msg);
+    callIt = false;
+    setTimeout(() => {
+      callIt = true;
+      messagesQueue.shift();
+      console.log(messagesQueue.length);
+    }, delayTime);
+  } else {
+    setTimeout(() => {
+      prepareMessage();
+    }, delayTime);
+  }
+}
 
-// CONNTECT TO ROCKET.CHAT WORKSPACE
-const runbot = async () => {
-  const conn = await driver.connect({ host: HOST, useSsl: SSL });
-  myuserid = await driver.login({ username: USER, password: PASS });
-  const roomsJoined = await driver.joinRooms(ROOMS);
-  const joinedRoom = await getRoomId(ROOMS[0]);
-  const subscribed = await driver.subscribeToMessages();
-};
+// SEND THE MESSAGE TO ROCKET.CHAT
+async function sendToRocket(msg) {
+  let isLarge;
+  let attachments = {
+    attachments: [{}],
+  };
+  let attachment = attachments.attachments[0];
+  let msgContent = {
+    msg: msg.caption,
+    rid: ROOMID,
+  };
 
-async function sendMessage(msg) {
-  // SENDING MESSAGE
-  console.log("sending message");
-  console.log(msg);
   if (msg.photo) {
+    if (!msg.photo[3]?.file_id) return;
     // MESSAGE HAS PHOTO
-    let url = await bot.getFileLink(msg.photo[3].file_id);
-    const sentMsg = await driver.sendMessage({
-      msg: msg.caption,
-      rid: ROOMID,
-      file: {
-        type: "mp4,",
-      },
-      attachments: [
-        {
-          image_url: url,
-        },
-      ],
+
+    isLarge = calculateSize(msg.photo);
+    if (!isLarge) {
+      let url = await bot.getFileLink(msg.photo[3].file_id);
+      attachment.image_url = url;
+    } else attachment.text = "PHOTO TOO LARGE TO SHARE HERE";
+    await sendMessage({
+      ...msgContent,
+      ...attachments,
     });
   } else if (msg.video) {
     // MESSAGE HAS VIDEO
-    let videoUrl = await bot.getFileLink(msg.video.file_id);
-    let fileName = videoUrl.slice(87);
-    const sentMsg = await driver.sendMessage({
-      msg: msg.caption,
-      rid: ROOMID,
-      attachments: [
-        {
-          video_url: videoUrl,
-          thumb_url: thumbUrl,
-          title: fileName,
-        },
-      ],
+
+    isLarge = calculateSize(msg.video);
+    if (!isLarge) {
+      let videoUrl = await bot.getFileLink(msg.video.file_id);
+      let fileName = videoUrl.slice(87);
+      attachment.video_url = videoUrl;
+      attachment.title = fileName;
+    } else attachment.text = "VIDEO TOO LARGE TO SHARE HERE";
+    await sendMessage({
+      ...msgContent,
+      ...attachments,
     });
   } else if (msg.document) {
     // MESSAGE HAS DOCUMENT
-    let fileUrl = await bot.getFileLink(msg.document.file_id);
-    let fileName = msg.document.file_name;
-    let fileType = msg.document.mime_type;
-    console.log(fileType.slice(0, -4));
-    let shrek;
-    shrek =
-      fileType === "image/jpeg"
-        ? { image_url: fileUrl }
-        : fileType === "video/mp4"
-        ? { video_url: fileUrl }
-        : { text: fileUrl };
-    const sentMsg = await driver.sendMessage({
-      msg: msg.caption,
-      rid: ROOMID,
-      attachments: [
-        {
-          ...shrek,
-          title: fileName,
-        },
-      ],
+
+    isLarge = calculateSize(msg.document);
+    if (!isLarge) {
+      let fileUrl = await bot.getFileLink(msg.document.file_id);
+      let fileName = msg.document.file_name;
+      let fileType = msg.document.mime_type;
+      let url;
+      url =
+        fileType === "image/jpeg"
+          ? { image_url: fileUrl }
+          : fileType === "video/mp4"
+          ? { video_url: fileUrl }
+          : { text: fileUrl };
+      attachments = {
+        attachments: [
+          {
+            ...url,
+            title: fileName,
+          },
+        ],
+      };
+    } else attachment.text = "FILE TOO LARGE TO SHARE HERE";
+    await sendMessage({
+      ...msgContent,
+      ...attachments,
     });
   } else {
     // TEXT ONLY
-    const botmsg = await driver.sendToRoom(msg.text, ROOMS[0]);
+    await sendToRoom(msg.text, ROOMS[0]);
   }
+}
+
+function calculateSize(el) {
+  return el.file_size / 1000000 > 20;
 }
 
 runbot();
